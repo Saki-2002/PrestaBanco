60d5e793d2175c33e24ba51053b59f04448f6528
Saki-2002 <isaguila2002@gmail.com>
2024-11-04 15:43:44 -0600

    MCApplicationService

--- src/main/java/com/prestabanco/PrestaBanco/Services/MCApplicationService.java
+++ src/main/java/com/prestabanco/PrestaBanco/Services/MCApplicationService.java
@@ -1,4 +1,19 @@
 package com.prestabanco.PrestaBanco.Services;
 
+import com.prestabanco.PrestaBanco.Entities.MCApplicationEntity;
+import com.prestabanco.PrestaBanco.Repositories.MCApplicationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MCApplicationService {
+
+    @Autowired
+    MCApplicationRepository mcApplicationRepository;
+
+    public MCApplicationEntity saveMCApplication(MCApplicationEntity mcApplicationEntity) {
+        return mcApplicationRepository.save(mcApplicationEntity);
+    }
+
 }
