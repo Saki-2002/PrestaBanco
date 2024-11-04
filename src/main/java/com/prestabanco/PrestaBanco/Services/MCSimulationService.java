2f893a52076bef343b599c6e973620792ffefb48
Saki-2002 <isaguila2002@gmail.com>
2024-11-04 16:00:19 -0600

    MCSimulationService

--- src/main/java/com/prestabanco/PrestaBanco/Services/MCSimulationService.java
+++ src/main/java/com/prestabanco/PrestaBanco/Services/MCSimulationService.java
@@ -1,4 +1,24 @@
 package com.prestabanco.PrestaBanco.Services;
 
+import org.springframework.stereotype.Service;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+@Service
 public class MCSimulationService {
+
+    public double simulateMortgageCredit(int loanAmount, int loanTerm, double annualInterestRate){
+
+        double r = (annualInterestRate/12)/100;
+        int n = loanTerm*12;
+        double aux=Math.pow((1+r),n);
+        double M = (loanAmount*((r*aux)/(aux-1)));
+
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setDecimalSeparator('.');
+        DecimalFormat df = new DecimalFormat("#.##", symbols);
+        return Double.parseDouble(df.format(M));
+    }
+
 }
