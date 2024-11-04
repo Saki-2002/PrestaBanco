b41204b075877337a55608cb4db4498ec25ef326
Saki-2002 <isaguila2002@gmail.com>
2024-11-04 17:17:17 -0600

    MCTypesService

--- src/main/java/com/prestabanco/PrestaBanco/Services/MCTypesService.java
+++ src/main/java/com/prestabanco/PrestaBanco/Services/MCTypesService.java
@@ -1,4 +1,82 @@
 package com.prestabanco.PrestaBanco.Services;
 
+import com.prestabanco.PrestaBanco.Entities.MCTypesEntity;
+import com.prestabanco.PrestaBanco.Repositories.MCTypesRepository;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MCTypesService {
+
+    @Autowired
+    MCTypesRepository mcTypesRepository;
+
+    public List<MCTypesEntity> getAll() {
+        return new ArrayList<>(mcTypesRepository.findAll());
+    }
+
+    @PostConstruct
+    public void init() {
+        createIfNotFound_1stHome();
+        createIfNotFound_2ndHome();
+        createIfNotFound_commProp();
+        createIfNotFound_remodeling();
+    }
+
+    private void createIfNotFound_1stHome(){
+        if(mcTypesRepository.findByType("Primera Vivienda")==null){
+            mcTypesRepository.save(new MCTypesEntity(
+                    null,
+                    "Primera Vivienda",
+                    30,
+                    0.035,
+                    0.05,
+                    0.8
+            ));
+        }
+    }
+
+    private void createIfNotFound_2ndHome(){
+        if(mcTypesRepository.findByType("Segunda Vivienda")==null){
+            mcTypesRepository.save(new MCTypesEntity(
+                    null,
+                    "Segunda Vivienda",
+                    20,
+                    0.04,
+                    0.06,
+                    0.7
+            ));
+        }
+    }
+
+    private void createIfNotFound_commProp(){
+        if(mcTypesRepository.findByType("Propiedades Comerciales")==null){
+            mcTypesRepository.save(new MCTypesEntity(
+                    null,
+                    "Propiedades Comerciales",
+                    25,
+                    0.05,
+                    0.07,
+                    0.6
+            ));
+        }
+    }
+
+    private void createIfNotFound_remodeling(){
+        if(mcTypesRepository.findByType("Remodelación")==null){
+            mcTypesRepository.save(new MCTypesEntity(
+                    null,
+                    "Remodelación",
+                    15,
+                    0.045,
+                    0.06,
+                    0.5
+            ));
+        }
+    }
 }
